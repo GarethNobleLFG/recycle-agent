65d9c60ab180fe0ca58d061fa8883b9cb901dd3e
Shine Zaw <141216177+shinekhantzaw@users.noreply.github.com>
2026-02-06 19:28:44 -0500

    Init Commit (#3)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h3 className="text-2xl font-semibold">Machine Learning Project</h3>
+    </div>
+  )
+}
+
+export default App
